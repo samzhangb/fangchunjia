e32b06069a04033d40e7ee67dbaaaeea1558ab7a
B Zhang <samzhangb@gmail.com>
2025-05-04 20:59:24 +0200

    Initial Commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+export const routes: Routes = [
+    {
+        path: 'portfolio',
+        component: PortfolioComponent,
+    }
+];
